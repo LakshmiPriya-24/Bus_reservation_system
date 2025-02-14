4e981fe908659137d5e4e56468d6708283baa6c2
Lakshmi <lakshmipriya12a1@gmail.com>
2025-02-15 00:57:06 +0530

    updated

--- src/BusResv/DbConnection.java
+++ src/BusResv/DbConnection.java
@@ -4,7 +4,7 @@ import java.sql.*;
 public class DbConnection {
 	private static final String url = "jdbc:mysql://localhost:3306/busresv";
 	private static final String userName = "root";
-	private static final String passWord = "Lachu@mysql25";
+	private static final String passWord = "test";
 	
 	public static Connection getConnection() throws SQLException{
 		return  DriverManager.getConnection(url,userName,passWord);
